cec1033a79609e1cc105fe5f127845cce78389aa
micheal <micheal.s.samir@gmail.com>
2026-02-28 13:28:12 +0200

    defaults as pint and swagger

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/documetation', function () {
+    // Define which documentation to use, defaulting to 'default' if not set
+    $documentation = config('l5-swagger.documentation') ?? 'default';
+    $useAbsolutePath = true;
 
+    return view('vendor.l5-swagger.index', compact('documentation', 'useAbsolutePath'));
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
